9ad70f4dbd9f90609136f54e069f24673fccf881
Marcos Aurelio <aureliowozhiak@gmail.com>
2023-10-18 19:08:39 -0300

    v1 of pagination

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s: %(message)s",
 )
 
-config_path = "config_files"
+config_path = "../config_files"
 filesaver = FileSavers()
 
 for filename in os.listdir(config_path):
@@ -31,88 +31,90 @@ for filename in os.listdir(config_path):
             config_file = json.load(file)
 
         for url, v in config_file.items():
-            if "webscrapper" in filename:
-                logging.info(f"Executando web scraping para a URL: {url}")
-                webscrapper = WebPageDataScrappers(url)
-                for i in v:
-                    filesaver.save_content(
-                        webscrapper.handle_content(i["tag"], i["attrs"]),
-                        i["file_name"],
-                        i["attrs"],
-                        sep=i["sep"],
-                    )
+            #if "webscrapper" in filename:
+            #    logging.info(f"Executando web scraping para a URL: {url}")
+            #    webscrapper = WebPageDataScrappers(url)
+            #    for i in v:
+            #        filesaver.save_content(
+            #            webscrapper.handle_content(i["tag"], i["attrs"]),
+            #            i["file_name"],
+            #            i["attrs"],
+            #            sep=i["sep"],
+            #        )
 
             if "api" in filename:
                 logging.info(f"Fazendo solicitação API para a URL: {url}")
                 for i in v:
-                    response = json.dumps(
+                    page = i["page"] if i["page"] != "" else None
+                    response = str(
                         ApiRequests(url, headers=i["api_headers"])
-                        .make_request(i["endpoint_path"])
-                        .json()
-                    )
+                        .make_full_request(i["endpoint_path"],
+                                           pagination=page)
+                            )
+                        
                     filesaver.save_content(response, i["file_name"])
 
-            if "postgres" in filename:
-                logging.info(f"Conectando ao banco de dados host={url}")
-                host = url
-                port = v["port"]
-                user = v["user"]
-                password = v["password"]
-                db_name = v.get("db_name")  # opcional
-                connection_string = build_postgres_url(
-                    user, password, host, port, db_name
-                )
-                connector = DatabaseConnector(connection_string)
-                connector.connect()
-                for table, table_config in v["tables"].items():
-                    table_data = connector.extract(
-                        table,
-                        table_config.get("columns"),
-                        table_config.get("where"),
-                        table_config.get("limit"),
-                    )
-                    filesaver.save_content(
-                        table_data, table_config["filename"]
-                    )
-
-            if "mysql" in filename:
-                logging.info(f"Conectando ao banco de dados host={url}")
-                host = url
-                port = v["port"]
-                user = v["user"]
-                password = v["password"]
-                db_name = v.get("db_name")  # opcional
-                connection_string = build_mysql_url(
-                    user, password, port, host, db_name
-                )
-                connector = DatabaseConnector(connection_string)
-                connector.connect()
-                for table, table_config in v["tables"].items():
-                    table_data = connector.extract(
-                        table,
-                        table_config.get("columns"),
-                        table_config.get("where"),
-                        table_config.get("limit"),
-                    )
-                    filesaver.save_content(
-                        table_data, table_config["filename"]
-                    )
-
-            if "sqlite" in filename:
-                logging.info(f"Conectando ao banco sqlite com path {url}")
-                connection_string = build_sqlite_url(url)
-                connector = DatabaseConnector(connection_string)
-                connector.connect()
-                for table, table_config in v["tables"].items():
-                    table_data = connector.extract(
-                        table,
-                        table_config.get("columns"),
-                        table_config.get("where"),
-                        table_config.get("limit"),
-                    )
-                    filesaver.save_content(
-                        table_data, table_config["filename"]
-                    )
+            #if "postgres" in filename:
+            #    logging.info(f"Conectando ao banco de dados host={url}")
+            #    host = url
+            #    port = v["port"]
+            #    user = v["user"]
+            #    password = v["password"]
+            #    db_name = v.get("db_name")  # opcional
+            #    connection_string = build_postgres_url(
+            #        user, password, host, port, db_name
+            #    )
+            #    connector = DatabaseConnector(connection_string)
+            #    connector.connect()
+            #    for table, table_config in v["tables"].items():
+            #        table_data = connector.extract(
+            #            table,
+            #            table_config.get("columns"),
+            #            table_config.get("where"),
+            #            table_config.get("limit"),
+            #        )
+            #        filesaver.save_content(
+            #            table_data, table_config["filename"]
+            #        )
+#
+            #if "mysql" in filename:
+            #    logging.info(f"Conectando ao banco de dados host={url}")
+            #    host = url
+            #    port = v["port"]
+            #    user = v["user"]
+            #    password = v["password"]
+            #    db_name = v.get("db_name")  # opcional
+            #    connection_string = build_mysql_url(
+            #        user, password, port, host, db_name
+            #    )
+            #    connector = DatabaseConnector(connection_string)
+            #    connector.connect()
+            #    for table, table_config in v["tables"].items():
+            #        table_data = connector.extract(
+            #            table,
+            #            table_config.get("columns"),
+            #            table_config.get("where"),
+            #            table_config.get("limit"),
+            #        )
+            #        filesaver.save_content(
+            #            table_data, table_config["filename"]
+            #        )
+#
+            #if "sqlite" in filename:
+            #    logging.info(f"Conectando ao banco sqlite com path {url}")
+            #    connection_string = build_sqlite_url(url)
+            #    connector = DatabaseConnector(connection_string)
+            #    connector.connect()
+            #    for table, table_config in v["tables"].items():
+            #        table_data = connector.extract(
+            #            table,
+            #            table_config.get("columns"),
+            #            table_config.get("where"),
+            #            table_config.get("limit"),
+            #        )
+            #        filesaver.save_content(
+            #            table_data, table_config["filename"]
+            #        )
 
 # Feche o arquivo de log após o término do programa
 logging.shutdown()
--- src/methods/extractors/apiRequests.py
+++ src/methods/extractors/apiRequests.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class ApiRequests:
@@ -8,7 +9,39 @@ class ApiRequests:
 
     def make_request(self, endpoint, method="GET", params=None, data=None):
         url = f"{self.base_url}/{endpoint}"
+        print(url)
         response = requests.request(
             method, url, headers=self.headers, params=params, data=data
         )
         return response
+    
+    #usign make_request method base create a pagination method passing the endpoint and a dict like this:
+    #  "page":
+    #           {
+    #               "page_start": 1,
+    #               "page_step":1,
+    #               "page_end": 3
+    #           }
+    #   "page_start" is the first page to be requested
+    #   "page_step" is the step between pages
+    #   "page_end" is the last page to be requested
+    #   "page_start" and "page_end" are inclusive
+    def make_full_request(
+        self, endpoint, method="GET", params=None, data=None, pagination=None
+    ):
+        if pagination is None:
+            return self.make_request(endpoint, method, params, data)
+
+        page_start = pagination["page_start"]
+        page_step = pagination["page_step"]
+        page_end = pagination["page_end"]
+
+        responses = {}
+        for page in range(page_start, page_end + 1, page_step):
+            page_params = {"page": page}
+            response = self.make_request(endpoint, method, page_params, data)
+            responses[page] = response.json()
+            
+        return responses
+    
+
